365b367241044d2b4043526e39fb7f93bff46e19
Gemagit <gemitaphotographer@gmail.com>
2024-04-29 15:34:55 +0200

    createCardandThemeContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import {ThemeContext} from './context/themeContext'
 //import {UserProvider} from './context/userContext'
 import Formulario from './components/MainComponent/Form'
+import Card from './components/MainComponent/Card'
 import './App.css'
 
 function App() {
- 
 
+//const [theme, setTheme]= useState('')
+
+ /*  //el objeto que representa el contexto
+ const themeData = {
+  theme,
+  toggleTheme
+ }
+ */
   return (
     <>
-    
-      <div>
+      <ThemeContext.Provider /*</>value={themeData}*/>
         <Formulario />
-      </div>
-    
+        <Card />
+      </ThemeContext.Provider>
     </>
   )
 }
